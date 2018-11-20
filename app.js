5a90db349fde5bbd9a20ed183130d21363c162a5
BengBu-YueZhang <zhangyue_coder@163.com>
2018-11-20 13:37:38 +0800

    update

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const Router = require('koa-router')
 const app = new Koa()
 const router = new Router()
 
-const views = require('koa-views')
 const co = require('co')
 const convert = require('koa-convert')
 const json = require('koa-json')
@@ -18,36 +17,20 @@ const routes = require('./routes')
 
 const port = process.env.PORT || config.port
 
-// error handler
 onerror(app)
 
-// middlewares
 app.use(bodyparser())
   .use(json())
   .use(logger())
   .use(require('koa-static')(__dirname + '/public'))
-  .use(views(path.join(__dirname, '/views'), {
-    options: {settings: {views: path.join(__dirname, 'views')}},
-    map: {'njk': 'nunjucks'},
-    extension: 'njk'
-  }))
   .use(router.routes())
   .use(router.allowedMethods())
 
-// logger
 app.use(async (ctx, next) => {
   const start = new Date()
   await next()
   const ms = new Date() - start
   console.log(`${ctx.method} ${ctx.url} - $ms`)
-})
-
-router.get('/', async (ctx, next) => {
-  // ctx.body = 'Hello World'
-  ctx.state = {
-    title: 'Koa2'
-  }
-  await ctx.render('index', ctx.state)
 })
 
 routes(router)
