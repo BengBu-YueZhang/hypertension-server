ab49c35f02edefe61627b6b24991676f4c8a8892
BengBu-YueZhang <zhangyue_coder@163.com>
2018-11-22 11:52:36 +0800

    update

--- controller/User.Controller.js
+++ controller/User.Controller.js
@@ -1,9 +1,20 @@
 const UserModel = require('../models/User.Model')
+const bcrypt = require('../util/bcrypt')
 
 module.exports = {
   async addUser (ctx, next) {
-  },
-
-  async isHaveUser (ctx, next) {
+    try {
+      const name = 'root'
+      const password = 'zy199467'
+      let user = await UserModel.findOne({ name })
+      if (user) ctx.throw(400, new Error('用户名已被注册'))
+      password = bcrypt.encrypt(password)
+      user = new UserModel({ name, password })
+      await user.save()
+    } catch (error) {
+      ctx.throw(error)
+    } finally {
+      await next()
+    }
   }
 }
